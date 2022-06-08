47cb6061bada2e603adae9c727c141c89ba346d5
Spencer Sevilla <spencer.builds.networks@gmail.com>
2022-06-07 21:19:40 -0700

    first draft of session-set-delete PFCP messaging for smf and upf (not sgwc/u)

--- src/smf/n4-handler.c
+++ src/smf/n4-handler.c
@@ -1093,6 +1093,22 @@ uint8_t smf_epc_n4_handle_session_deletion_response(
     return OGS_PFCP_CAUSE_REQUEST_ACCEPTED;
 }
 
+void smf_epc_n4_handle_session_set_deletion_response(
+        ogs_pfcp_node_t *node, ogs_pfcp_xact_t *xact,
+        ogs_pfcp_session_set_deletion_response_t *req)
+{
+    upf_sess_t *sess = NULL, *next = NULL;;
+    ogs_assert(node);
+    ogs_assert(xact);
+    ogs_assert(req);
+
+    ogs_debug("Session Set Deletion Response");
+
+    ogs_list_for_each_safe(node->sess_list, next, sess) {
+        smf_sess_remove(sess);
+    }
+}
+
 void smf_n4_handle_session_report_request(
         smf_sess_t *sess, ogs_pfcp_xact_t *pfcp_xact,
         ogs_pfcp_session_report_request_t *pfcp_req)
