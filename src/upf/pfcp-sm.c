d84e54a146013301ec431b8421d9dfbeac7f0d30
Spencer Sevilla <spencer.builds.networks@gmail.com>
2022-08-25 11:46:00 -0700

    add addr/port to pfcp assoc/de-assoc logs (#40)
    pfcp association log adds addr/port

--- src/upf/pfcp-sm.c
+++ src/upf/pfcp-sm.c
@@ -170,7 +170,9 @@ void upf_pfcp_state_associated(ogs_fsm_t *s, upf_event_t *e)
 
     switch (e->id) {
     case OGS_FSM_ENTRY_SIG:
-        ogs_info("PFCP associated");
+        ogs_info("PFCP associated [%s]:%d",
+            OGS_ADDR(&node->addr, buf),
+            OGS_PORT(&node->addr));
         ogs_timer_start(node->t_no_heartbeat,
                 ogs_app()->time.message.pfcp.no_heartbeat_duration);
 
@@ -178,7 +180,9 @@ void upf_pfcp_state_associated(ogs_fsm_t *s, upf_event_t *e)
 
         break;
     case OGS_FSM_EXIT_SIG:
-        ogs_info("PFCP de-associated");
+        ogs_info("PFCP de-associated [%s]:%d",
+            OGS_ADDR(&node->addr, buf),
+            OGS_PORT(&node->addr));
         ogs_timer_stop(node->t_no_heartbeat);
 
         stats_update_upf_pfcp_nodes();
@@ -205,12 +209,16 @@ void upf_pfcp_state_associated(ogs_fsm_t *s, upf_event_t *e)
                     &message->pfcp_heartbeat_response));
             break;
         case OGS_PFCP_ASSOCIATION_SETUP_REQUEST_TYPE:
-            ogs_warn("PFCP[REQ] has already been associated");
+            ogs_warn("PFCP[REQ] has already been associated [%s]:%d",
+                OGS_ADDR(&node->addr, buf),
+                OGS_PORT(&node->addr));
             ogs_pfcp_up_handle_association_setup_request(node, xact,
                     &message->pfcp_association_setup_request);
             break;
         case OGS_PFCP_ASSOCIATION_SETUP_RESPONSE_TYPE:
-            ogs_warn("PFCP[RSP] has already been associated");
+            ogs_warn("PFCP[RSP] has already been associated [%s]:%d",
+                OGS_ADDR(&node->addr, buf),
+                OGS_PORT(&node->addr));
             ogs_pfcp_up_handle_association_setup_response(node, xact,
                     &message->pfcp_association_setup_response);
             break;
