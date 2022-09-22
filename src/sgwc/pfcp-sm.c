b1de3a4fd1dc56e66a1ddb869953da503d22e030
Spencer Sevilla <spencer.builds.networks@gmail.com>
2022-09-21 21:23:45 -0700

    [pfcp] adding logic to detect pfcp association failures and reestablish sessions

--- src/sgwc/pfcp-sm.c
+++ src/sgwc/pfcp-sm.c
@@ -175,6 +175,8 @@ void sgwc_pfcp_state_associated(ogs_fsm_t *s, sgwc_event_t *e)
         ogs_timer_start(node->t_no_heartbeat,
                 ogs_app()->time.message.pfcp.no_heartbeat_duration);
 
+        sgwc_pfcp_resend_established_sessions(node);
+
         stats_update_sgwc_pfcp_nodes();
 
         break;
@@ -221,6 +223,7 @@ void sgwc_pfcp_state_associated(ogs_fsm_t *s, sgwc_event_t *e)
                 OGS_PORT(&node->addr));
             ogs_pfcp_cp_handle_association_setup_request(node, xact,
                     &message->pfcp_association_setup_request);
+            sgwc_pfcp_resend_established_sessions(node);
             break;
         case OGS_PFCP_ASSOCIATION_SETUP_RESPONSE_TYPE:
             ogs_warn("PFCP[RSP] has already been associated [%s]:%d",
@@ -228,6 +231,7 @@ void sgwc_pfcp_state_associated(ogs_fsm_t *s, sgwc_event_t *e)
                 OGS_PORT(&node->addr));
             ogs_pfcp_cp_handle_association_setup_response(node, xact,
                     &message->pfcp_association_setup_response);
+            sgwc_pfcp_resend_established_sessions(node);
             break;
         case OGS_PFCP_SESSION_ESTABLISHMENT_RESPONSE_TYPE:
             if (!message->h.seid_presence) ogs_error("No SEID");
