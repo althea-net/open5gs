b31427b67cba4a46bf349c2d92ea698685b9812f
Spencer Sevilla <spencer.builds.networks@gmail.com>
2023-02-03 14:35:08 -0800

    [smf] state machine improvements (#76)

--- src/smf/context.h
+++ src/smf/context.h
@@ -434,6 +434,11 @@ typedef struct smf_sess_s {
 
     bool active;
 
+    bool teardown_pfcp;
+    bool teardown_gx;
+    bool teardown_gy;
+    bool teardown_gtp;
+
     smf_ue_t *smf_ue;
 
     bool n1_released;
