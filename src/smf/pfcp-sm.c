85202b285dbdec534bdcb1a7220403e1013e0eba
Spencer Sevilla <spencer.builds.networks@gmail.com>
2024-01-04 12:43:40 -0800

    remove already_associated variable

--- src/smf/pfcp-sm.c
+++ src/smf/pfcp-sm.c
@@ -177,10 +177,7 @@ void smf_pfcp_state_associated(ogs_fsm_t *s, smf_event_t *e)
         ogs_timer_start(node->t_no_heartbeat,
                 ogs_app()->time.message.pfcp.no_heartbeat_duration);
 
-        if (node->already_associated) {
-            smf_epc_pfcp_resend_established_sessions(node);
-        }
-        node->already_associated = true;
+        smf_epc_pfcp_resend_established_sessions(node);
 
         stats_update_smf_pfcp_nodes();
 
