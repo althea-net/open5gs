1f178c9d821a59b6aa55acdb0f9957ac91fd0690
Justin Kilpatrick <justin@althea.net>
2024-01-27 21:35:01 -0500

    Don't panic when we're about to null these anyways

--- src/mme/mme-context.c
+++ src/mme/mme-context.c
@@ -2929,16 +2929,16 @@ void enb_ue_source_deassociate_target(enb_ue_t *enb_ue)
         source_ue = enb_ue;
         target_ue = enb_ue->target_ue;
 
-        ogs_assert(source_ue->target_ue);
-        ogs_assert(target_ue->source_ue);
+        //ogs_assert(source_ue->target_ue);
+        //ogs_assert(target_ue->source_ue);
         source_ue->target_ue = NULL;
         target_ue->source_ue = NULL;
     } else if (enb_ue->source_ue) {
         target_ue = enb_ue;
         source_ue = enb_ue->source_ue;
 
-        ogs_assert(source_ue->target_ue);
-        ogs_assert(target_ue->source_ue);
+        //ogs_assert(source_ue->target_ue);
+        //ogs_assert(target_ue->source_ue);
         source_ue->target_ue = NULL;
         target_ue->source_ue = NULL;
     }
